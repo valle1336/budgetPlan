6070c356b69002b92699f666a5888f09909184ae
valle1336 <7alexxi@gmail.com>
2025-07-24 11:37:03 +0200

    Initial commit

---
+++ src/test/java/com/lokakatt/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokakatt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
